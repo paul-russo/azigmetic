8e225cccb8ddc5f7d13407666d4afacb2b36b65e
Paul Russo <paul@paulrusso.me>
2021-11-14 19:13:46 -0700

    Add binding power

--- src/parser.zig
+++ src/parser.zig
@@ -1,12 +1,45 @@
 const std = @import("std");
 const Token = @import("token.zig").Token;
+const TokenTag = @import("token.zig").TokenTag;
 const S = @import("s.zig").S;
 
-pub fn parseTokens(tokens: []const Token) S {
-    // TODO
-    std.debug.print("{s}", .{tokens});
+const ParseError = error{
+    UnsupportedOperation,
+    UnsupportedToken,
+};
 
-    return S{ .atom = 2.0 };
+const InfixBP = struct {
+    left: u8,
+    right: u8,
+};
+
+fn getInfixBindingPower(op: u8) !InfixBP {
+    return switch (op) {
+        '+', '-' => .{ .left = 1, .right = 2 },
+        '*', '/' => .{ .left = 3, .right = 4 },
+        else => ParseError.UnsupportedOperation,
+    };
+}
+
+pub fn parseTokens(tokens: []const Token) !S {
+    var lhs = switch (tokens[0]) {
+        TokenTag.value => |value| S{ .atom = value },
+        else => S{ .atom = 0 },
+    };
+
+    for (tokens) |token| {
+        var op = switch (token) {
+            TokenTag.eof => break,
+            TokenTag.op => |op| op,
+            TokenTag.value => continue,
+        };
+
+        var bp = try getInfixBindingPower(op);
+
+        std.debug.print("{}", .{bp});
+    }
+
+    return lhs;
 }
 
 test "expect parseTokens to return (+ 1 (* 2 3)) for 1 + 2 * 3" {
@@ -37,5 +70,5 @@ test "expect parseTokens to return (+ 1 (* 2 3)) for 1 + 2 * 3" {
         },
     };
 
-    try std.testing.expectEqual(expectedS, parseTokens(testTokens[0..]));
+    try std.testing.expectEqual(expectedS, try parseTokens(testTokens[0..]));
 }
