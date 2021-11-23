9094bb108c0af4d72b3feb54b444dba6fcc14fb3
Paul Russo <paul@paulrusso.me>
2021-11-23 02:26:50 -0700

    Add additional evaluator test case

--- src/evaluator.zig
+++ src/evaluator.zig
@@ -20,3 +20,48 @@ pub fn evaluateExpression(expression: S) EvaluatorError!f64 {
         else => EvaluatorError.InvalidExpression,
     };
 }
+
+// TESTS
+test "expect result of (+ 1 (* 2 3)) to be 7" {
+    const testExpression = S{
+        .cons = .{
+            .head = '+',
+            .rest = &[_]S{
+                S{ .atom = 1.0 },
+                S{
+                    .cons = .{
+                        .head = '*',
+                        .rest = &[_]S{
+                            S{ .atom = 2.0 },
+                            S{ .atom = 3.0 },
+                        },
+                    },
+                },
+            },
+        },
+    };
+
+    var result = try evaluateExpression(testExpression);
+    try std.testing.expectEqual(result, 7);
+}
+
+test "expect result of (+ (- (/ (* 64.2 1.5) 9.567) 4.3) 1.09) to be 7" {
+    const testExpression = S{
+        .cons = .{ .head = '+', .rest = &[_]S{
+            S{ .cons = .{ .head = '-', .rest = &[_]S{
+                S{ .cons = .{ .head = '/', .rest = &[_]S{
+                    S{ .cons = .{ .head = '*', .rest = &[_]S{
+                        S{ .atom = 64.2 },
+                        S{ .atom = 1.5 },
+                    } } },
+                    S{ .atom = 9.567 },
+                } } },
+                S{ .atom = 4.3 },
+            } } },
+            S{ .atom = 1.09 },
+        } },
+    };
+
+    var result = try evaluateExpression(testExpression);
+    try std.testing.expectEqual(result, 6.85585136406397);
+}
