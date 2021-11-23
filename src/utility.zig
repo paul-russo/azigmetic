0e3bd838e41eb6079cd87db1a50ffd7523e8e68d
Paul Russo <paul@paulrusso.me>
2021-11-23 02:42:56 -0700

    Remove unused file

--- src/utility.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn stringify(comptime T: type, value: T) ![]u8 {
-    const str = try std.fmt.allocPrint(std.heap.page_allocator, "{s}", .{value});
-    defer std.heap.page_allocator.free(str);
-    return str;
-}
